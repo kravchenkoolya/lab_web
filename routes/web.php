8d39e1dc3d6587870337a9596ebe3b8a611ccf64
kravchenkoolya <olha.kravchenko@oa.edu.ua>
2023-06-13 00:16:42 +0200

    commit 13 lab

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,7 @@
 <?php
-use App\Http\Controllers\RestTestController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RestTestController;
 use App\Http\Controllers\DiggingDeeperController;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,25 +12,22 @@ use App\Http\Controllers\DiggingDeeperController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
     return view('welcome');
 });
-Route::resource('rest', RestTestController::class)->names('restTest');
-
-
-Route::group(['prefix' => 'digging_deeper'], function () {
-
-    Route::get('collections', [DiggingDeeperController::class, 'collections'])
-
-        ->name('digging_deeper.collections');
-
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
 });
-
+Route::resource('rest', RestTestController::class)->names('restTest');
 Route::group([ 'namespace' => 'App\Http\Controllers\Blog', 'prefix' => 'blog'], function () {
     Route::resource('posts', PostController::class)->names('blog.posts');
 });
-
 //Адмінка
 $groupData = [
     'namespace' => 'App\Http\Controllers\Blog\Admin',
@@ -43,16 +39,17 @@ Route::group($groupData, function () {
     Route::resource('categories', CategoryController::class)
         ->only($methods)
         ->names('blog.admin.categories');
-
     //BlogPost
     Route::resource('posts', PostController::class)
         ->except(['show'])                               //не робити маршрут для метода show
         ->names('blog.admin.posts');
 });
-
-
 Route::get('/collections', [DiggingDeeperController::class, 'collections']);
 Route::group(['prefix' => 'digging_deeper'], function () {
     Route::get('collections', [DiggingDeeperController::class, 'collections'])
         ->name('digging_deeper.collections');
+    Route::get('process-video', [DiggingDeeperController::class, 'processVideo'])
+        ->name('digging_deeper.processVideo');
+    Route::get('prepare-catalog', [DiggingDeeperController::class, 'prepareCatalog'])
+        ->name('digging_deeper.prepareCatalog');
 });
